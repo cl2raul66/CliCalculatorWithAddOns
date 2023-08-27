18888d4642ebaa92dd8d1b269cd90941ebf6a775
Raul Avila Catala <cl2raul66@hotmail.com>
2023-08-27 12:08:21 -0600

    using System.Reflection

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -1,108 +1,57 @@
 ﻿using System.Reflection;
-using System.Composition.Hosting;
-using Sample.Plugin;
 
-var a = typeof(Program).GetTypeInfo().Assembly;
-
-var configuration = new ContainerConfiguration();
-configuration.WithAssembly(a);
-
-// Load all DLLs in the current directory
-var dlls = Directory.GetFiles(Directory.GetCurrentDirectory(), "Plugin.*.dll");
-foreach (var dll in dlls)
+Console.WriteLine("Bienvenido a CliCalculadora!");
+while (true)
 {
-    var assembly = Assembly.LoadFile(dll);
-    configuration.WithAssembly(assembly);
-}
-
-using (var container = configuration.CreateContainer())
-{
-    Console.WriteLine("Choose an operation:");
-    Console.WriteLine("1. Add");
-
-    // Get all exported types that implement ICalculator
-    var calculatorTypes = container.GetExports<ICalculator>().Select(c => c.GetType()).Distinct().ToList();
-
-    // Display a menu option for each exported type
-    int i = 2;
-    foreach (var calculatorType in calculatorTypes)
+    Console.WriteLine("Selecciona una opción:");
+    Console.WriteLine("1. Sumar");
+    string[] dlls = Directory.GetFiles(".", "Plugin.*.dll");
+    for (int i = 0; i < dlls.Length; i++)
     {
-        var methods = calculatorType.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
-        foreach (var method in methods)
-        {
-            if (method.Name != "Add" && method.GetParameters().Length == 2 && method.GetParameters()[0].ParameterType == typeof(int) && method.GetParameters()[1].ParameterType == typeof(int))
-            {
-                Console.WriteLine(i + ". " + method.Name);
-                i++;
-            }
-        }
+        Console.WriteLine($"{i + 2}. {Path.GetFileNameWithoutExtension(dlls[i])}");
     }
-
-    int choice = Convert.ToInt32(Console.ReadLine());
-
-    Console.WriteLine("Enter first number: ");
-    int x = Convert.ToInt32(Console.ReadLine());
-
-    Console.WriteLine("Enter second number: ");
-    int y = Convert.ToInt32(Console.ReadLine());
-
-    if (choice == 1)
+    Console.WriteLine($"{dlls.Length + 2}. Salir");
+    string? opcion = Console.ReadLine();
+    if (opcion is null || opcion == (dlls.Length + 2).ToString())
     {
-        var calculator = container.GetExport<ICalculator>();
-        Console.WriteLine("Result: " + calculator.Add(x, y));
+        break;
+    }
+    Console.Write("Ingresa el primer operador: ");
+    double a = double.Parse(Console.ReadLine() ?? "0");
+    Console.Write("Ingresa el segundo operador: ");
+    double b = double.Parse(Console.ReadLine() ?? "0");
+    double resultado = 0;
+    if (opcion == "1")
+    {
+        resultado = Sumar(a, b);
     }
     else
     {
-        // Find the selected operation
-        string operation = null;
-        i = 2;
-        foreach (var calculatorType in calculatorTypes)
+        int index = int.Parse(opcion!) - 2;
+        if (index >= 0 && index < dlls.Length)
         {
-            var methods = calculatorType.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
-            foreach (var method in methods)
-            {
-                if (method.Name != "Add" && method.GetParameters().Length == 2 && method.GetParameters()[0].ParameterType == typeof(int) && method.GetParameters()[1].ParameterType == typeof(int))
-                {
-                    if (i == choice)
-                    {
-                        operation = method.Name;
-                        break;
-                    }
-                    i++;
-                }
-            }
-            if (operation != null) break;
-        }
-
-        if (operation == null)
-        {
-            Console.WriteLine("Invalid choice.");
+            resultado = CargarOperacion(dlls[index], a, b);
         }
         else
         {
-            // Find the calculator that implements the selected operation
-            object calculator = null;
-            foreach (var calculatorType in calculatorTypes)
-            {
-                var method = calculatorType.GetMethod(operation);
-                if (method != null)
-                {
-                    calculator = container.GetExports<ICalculator>().First(c => c.GetType() == calculatorType);
-                    break;
-                }
-            }
-
-            if (calculator == null)
-            {
-                Console.WriteLine(operation + " functionality is not available.");
-            }
-            else
-            {
-                // Invoke the selected operation
-                var method = calculator.GetType().GetMethod(operation);
-                double result = (double)method.Invoke(calculator, new object[] { x, y });
-                Console.WriteLine("Result: " + result);
-            }
+            Console.WriteLine("Opción inválida.");
+            continue;
         }
     }
+    Console.WriteLine($"\n El resultado es: {resultado}");
+}
+
+
+static double Sumar(double a, double b)
+{
+    return a + b;
+}
+
+static double CargarOperacion(string dll, double a, double b)
+{
+    Assembly assembly = Assembly.LoadFrom(dll);
+    Type? type = assembly.GetType(Path.GetFileNameWithoutExtension(dll) + ".Operacion");
+    MethodInfo? methodInfo = type?.GetMethod("Calcular");
+    object instance = Activator.CreateInstance(type);
+    return (double)methodInfo.Invoke(instance, new object[] { a, b });
 }
