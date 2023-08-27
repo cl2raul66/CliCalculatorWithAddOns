da0c7eb3167530cba4c5d988fd092de253067561
Raul Avila Catala <cl2raul66@hotmail.com>
2023-08-27 09:27:49 -0600

    Plugin for multiply function

---
+++ src/Plugin.Multiply/Plugin.Multiply/Multiplier.cs
@@ -0,0 +1,17 @@
+﻿using System.Composition;
+
+namespace Plugin.Multiply;
+
+public interface IMultiplier
+{
+    int Multiply(int x, int y);
+}
+
+[Export(typeof(IMultiplier))]
+public class Multiplier : IMultiplier
+{
+    public int Multiply(int x, int y)
+    {
+        return x * y;
+    }
+}
